b8d7aa03fd65744d2972efee2abc0ebe271b3065
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-08-22 23:07:33 +0300

    Initial Commit

---
+++ CarouselView/CarouselViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselViewApp.swift
+//  CarouselView
+//
+//  Created by Ahmet Bostancıklıoğlu on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarouselViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
